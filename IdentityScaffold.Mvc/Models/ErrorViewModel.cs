192a7846ee72d38439d3a5a5326c0bce86d1a37a
design <276667311@qq.comm>
2020-07-15 15:46:14 +0800

    添加项目文件。

---
+++ IdentityScaffold.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityScaffold.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
